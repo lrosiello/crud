46ab5a32f117e70b4984b3c149669e397e77d000
lrosiello <rosiello.paco@gmail.com>
2023-05-28 21:49:25 -0300

    CRUD de categorias completo
    faltan detalles.
    ahora sigue las capas, y otras restricciones para categorias en delete por existencia de capas

--- src/pages/api/categories.js
+++ src/pages/api/categories.js
@@ -1,51 +1,68 @@
 import { query } from "../../lib/db";
 
 export default async function handler(req, res) {
-
   let message;
-  
+  let category;
 
+
+  //GET ALL CATEGORIES
   if (req.method === "GET") {
     const categories = await query("SELECT * FROM categorias", []);
     res.status(200).json({ categories });
-  } else {
-    res.status(405).end();
-  }
-
-  if (req.method === "POST") {
-    let category;
+  } 
+  
+  //CREATE A NEW CATEGORY
+  else if (req.method === "POST") {
     const { categoryName, description, orderNumber } = req.body;
-    const addCategory = await query(
-      "INSERT INTO categorias (nombre_categoria,descripcion,numero_orden) VALUES($1, $2, $3)",
-      [categoryName, description, orderNumber]
-    );
 
-    if (addCategory.rows.length > 0) {
-      const categoryId = addCategory.rows[0].id;
-      if(categoryId){
-        message = "success";
-        category = {
-          id: categoryId,
-          nombre_categoria: categoryName,
-          descripcion: description,
-          numero_orden: orderNumber,
-        };
-      }else {
-        message = "error";
-        category = null;
-      } 
+    //VERIFIES THAT ALL INPUTS ARE FILLED
+    if (
+      categoryName !== null &&
+      categoryName!== "" &&
+      orderNumber !== null &&
+      orderNumber !== ""
+    ) {
+      //VERIFIES THAT THE CATEGORY DOES NOT EXISTS
+      const verifyName = await query(
+        "SELECT nombre_categoria FROM categorias WHERE nombre_categoria = $1",
+        [categoryName]
+      );
+      if (verifyName.rowCount === 0) {
+        //INSERT A NEW CATEGORY
+        const addCategory = await query(
+          "INSERT INTO categorias (nombre_categoria,descripcion,numero_orden) VALUES($1, $2, $3) RETURNING *",
+          [categoryName, description, orderNumber]
+        );
+        //IT TAKES DATA FROM THE CATEGORY ADDED
+        if (addCategory.rowCount > 0) {
+          const categoryId = addCategory.rows[0].id;
+          if (categoryId) {
+            message = "success";
+            category = {
+              id: categoryId,
+              nombre_categoria: categoryName,
+              descripcion: description,
+              numero_orden: orderNumber,
+            };
+          } else {
+            message = "error";
+            category = null;
+          }
+          //RESPONSES
+          res
+            .status(200)
+            .json({ response: { message: message, category: category } });
+        }
+      } else {
+        res
+          .status(500)
+          .json({ error: "That category already exists, could not create" });
+      }
+    } else {
+      res.status(500).json({
+        error: "Be sure that description and order number are filled",
+      });
+      res.status(405).end();
     }
-    res
-      .status(200)
-      .json({ response: { message: message, category: category } });
   }
-
-  
-
-
-
-  
 }
-
-
-
--- src/pages/api/categories/[id].js
+++ src/pages/api/categories/[id].js
@@ -3,51 +3,99 @@ import { query } from "../../../lib/db";
 export default async function handler(req, res) {
   let message;
 
+  //GET ELEMENT BY ID
 
-  
+  if (req.method === "GET") {
+    const categoryId = req.query.id;
+    if (isNaN(categoryId)) {
+      res.status(500).json({ error: "The id value is not valid" });
+    } else {
+      const getCategoryById = await query(
+        "SELECT * FROM categorias WHERE id = $1",
+        [categoryId]
+      );
+      if (getCategoryById.rowCount === 0) {
+        res.status(500).json({ error: "This category does not exist" });
+      } else {
+        res.status(200).json({ getCategoryById });
+      }
+    }
+  }
+
   if (req.method === "DELETE") {
     const categoryId = req.query.id;
 
-    const deleteCategory = await query(
-      "DELETE FROM categorias WHERE id = $1", [categoryId]
-    );
-
-    const rowCount = deleteCategory.rowCount;
-    if (rowCount > 0) {
-      message = "success";
+    if (isNaN(categoryId)) {
+      res.status(500).json({ error: "The id value is not valid" });
     } else {
-      message = "error";
+      const deleteCategory = await query(
+        "DELETE FROM categorias WHERE id = $1",
+        [categoryId]
+      );
+
+      const rowCount = deleteCategory.rowCount;
+      if (rowCount > 0) {
+        message = "category deleted succesfully";
+      } else {
+        message = "could not delete, this id does not exists";
+      }
+      res
+        .status(200)
+        .json({ response: { message: message, categoryId: categoryId } });
     }
-
-    res
-      .status(200)
-      .json({ response: { message: message, categoryId: categoryId } });
   }
-
 
   if (req.method === "PUT") {
     const categoryId = req.query.id;
-    const { categoryName, description, orderNumber } = req.body;
-  
-    const updateCategory = await query(
-      "UPDATE categorias SET nombre_categoria = $1, descripcion = $2, numero_orden = $3 WHERE id = $4",
-      [categoryName, description, orderNumber, categoryId]
-    );
-  
-    const rowCount = updateCategory.rowCount;
-    if (rowCount > 0) {
-      message = "success";
+
+    if (isNaN(categoryId)) {
+      res.status(500).json({ error: "The id value is not valid" });
     } else {
-      message = "error";
+      const { categoryName, description, orderNumber } = req.body;
+
+      //VERIFIES THAT THE CATEGORY DOES NOT EXISTS
+      const verifyName = await query(
+        "SELECT nombre_categoria FROM categorias WHERE nombre_categoria = $1",
+        [categoryName]
+      );
+      if (
+        categoryName !== null &&
+        categoryName !== "" &&
+        orderNumber !== null &&
+        orderNumber !== ""
+      ) {
+        if (verifyName.rowCount === 0) {
+          const updateCategory = await query(
+            "UPDATE categorias SET nombre_categoria = $1, descripcion = $2, numero_orden = $3 WHERE id = $4",
+            [categoryName, description, orderNumber, categoryId]
+          );
+
+          const rowCount = updateCategory.rowCount;
+          if (rowCount > 0) {
+            message = "category updated succesfully";
+          } else {
+            message = "error, could not update because id does not exists";
+          }
+
+          let category = {
+            id: categoryId,
+            nombre_categoria: categoryName,
+            descripcion: description,
+            numero_orden: orderNumber,
+          };
+          res
+            .status(200)
+            .json({ response: { message: message, category: category } });
+        } else {
+          res
+            .status(500)
+            .json({ error: "That category already exists, could not update" });
+        }
+      } else {
+        res.status(500).json({
+          error: "Be sure that description and order number are filled",
+        });
+      }
     }
-  
-    let category = {
-      id: categoryId,
-      nombre_categoria: categoryName,
-      descripcion: description,
-      numero_orden: orderNumber,
-    };
-    res.status(200).json({ response: { message: message, category: category } });
   }
-  
 }
